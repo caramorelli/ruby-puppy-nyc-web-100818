e9171722281a7347cbfb3971ff45bb65abcf3281
Cara Morelli <caramorelli7@gmail.com>
2018-10-10 19:27:12 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,30 @@
+
+
+class Dog 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end 
+  
+  def new 
+   @@all << self
+  end 
+  
+  def self.all 
+    @@all.each do |dog_name|
+      puts self.dog_name
+    end 
+  end 
+  
+  def self.clear_all
+    @@all = []
+  end 
+  
+  def name 
+    @name 
+  end 
+  
+  
+end 
